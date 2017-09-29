a08a4e446ea5527e8d1024af6a9fbb76f5b66139
Nina Louw <ninalouw@mac.com>
2017-09-29 00:57:50 -0700

    add second row of cards

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
         </div>
     </div>
     <!-- start of product cards -->
-    <h2 class="text-center">The Yoga Collection</h2>
+    <h2 class="text-center">The Luxe Women's Collection</h2>
     <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
             <div class="card">
             <img class="card-img-top" src="img/bra-1.jpeg" alt="Card image">
             <div class="card-body">
@@ -33,7 +33,7 @@
             </div>
         </div>
 
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
             <div class="card">
             <img class="card-img-top" src="img/hoodie-1.jpeg" alt="Card image">
             <div class="card-body">
@@ -45,7 +45,7 @@
             </div>
         </div>
 
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
             <div class="card">
             <img class="card-img-top" src="img/pants-1.jpeg" alt="Card image cap">
             <div class="card-body">
@@ -57,9 +57,88 @@
             </div>
         </div>
 
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
             <div class="card">
             <img class="card-img-top" src="img/pants-2.jpeg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Pants</h4>
+                <p class="card-text text-muted">The best pants ever.</p>
+                <p class="card-text">$88</p>
+                <a href="products.php" class="btn btn-primary">Buy</a>
+            </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- end of card section -->
+
+    <!-- larger image section -->
+    <div class="row center-container">
+        <div class="col-lg-5 col-md-5 col-sm-3 col-xs-6">
+            <div class="card">
+            <img class="card-img-top" src="img/top-md.jpeg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Women's Tops</h4>
+                <p class="card-text text-muted">Our best collection of all time.</p>
+                <a href="products.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-5 col-md-5 col-sm-3 col-xs-6">
+            <div class="card">
+            <img class="card-img-top" src="img/pants-md.jpeg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Women's Pants</h4>
+                <p class="card-text text-muted">Our best collection of all time.</p>
+                <a href="products.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- second card section -->
+    <h2 class="text-center">The Yoga Collection</h2>
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+            <div class="card">
+            <img class="card-img-top" src="img/bra-2.jpeg" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Sports Bra</h4>
+                <p class="card-text text-muted">The best sports bra ever.</p>
+                <p class="card-text">$50</p>
+                <a href="products.php" class="btn btn-primary">Buy</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+            <div class="card">
+            <img class="card-img-top" src="img/pants-5.jpeg" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Pants</h4>
+               <p class="card-text text-muted">The best pants ever.</p>
+                <p class="card-text">$90</p>
+                <a href="products.php" class="btn btn-primary">Buy</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+            <div class="card">
+            <img class="card-img-top" src="img/top-1.jpeg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Top</h4>
+               <p class="card-text text-muted">The best top ever.</p>
+                <p class="card-text">$97</p>
+                <a href="products.php" class="btn btn-primary">Buy</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+            <div class="card">
+            <img class="card-img-top" src="img/pants-7.jpeg" alt="Card image cap">
             <div class="card-body">
                 <h4 class="card-title">Pants</h4>
                 <p class="card-text text-muted">The best pants ever.</p>
