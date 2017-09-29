5f38ec66fd6cc43825133dc04081d3a0bffd5249
Nina Louw <ninalouw@mac.com>
2017-09-29 10:47:09 -0700

    set up sql tables, get products from db, display on products page

--- products.php
+++ products.php
@@ -7,67 +7,75 @@
         <div class="filters-products">
             <p class="lead"> Choose product categories to view products</p>
             <div class="filters-toggles">
-                <span class="filters-toggle active" data-product="men">Men</span>
-                <span class="filters-toggle active" data-product="women">Women</span>
-                <span class="filters-toggle active" data-product="bras">Sports Bras</span>
-                <span class="filters-toggle active" data-product="pants">Pants</span>
                 <span class="filters-toggle active" data-product="tops">Tops</span>
+                <span class="filters-toggle active" data-product="pants">Pants</span>
                 <span class="filters-toggle active" data-product="accessories">Accessories</span>
+                <span class="filters-toggle active" data-product="bras">Bras</span>
+                <span class="filters-toggle active" data-product="mats">Mats</span>
             </div>
         </div>
     </section>
     <!-- second card section -->
-    <h2 class="text-center">Some Dummy Products</h2>
-    <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-            <div class="card">
-            <img class="card-img-top" src="img/bra-2.jpeg" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-title">Sports Bra</h4>
-                <p class="card-text text-muted">The best sports bra ever.</p>
-                <p class="card-text">$50</p>
-                <a href="products.php" class="btn btn-primary">Buy</a>
-            </div>
-            </div>
-        </div>
+    <h2 class="text-center">Our Products</h2>
+    <?php 
+        include "backend/db.php";
+        
+        db_connect();
 
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-            <div class="card">
-            <img class="card-img-top" src="img/pants-5.jpeg" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-title">Pants</h4>
-            <p class="card-text text-muted">The best pants ever.</p>
-                <p class="card-text">$90</p>
-                <a href="products.php" class="btn btn-primary">Buy</a>
-            </div>
-            </div>
-        </div>
+        if(db_connect()) {
+            $query = " SELECT * FROM product_tb
+                      INNER JOIN category_tb
+                      ON product_tb.id=category_tb.product_id
+                      ORDER BY id ";
+            $queryResult = mysqli_query(db_connect(), $query);
+            // var_dump($queryResult);
 
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-            <div class="card">
-            <img class="card-img-top" src="img/top-1.jpeg" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title">Top</h4>
-            <p class="card-text text-muted">The best top ever.</p>
-                <p class="card-text">$97</p>
-                <a href="products.php" class="btn btn-primary">Buy</a>
-            </div>
-            </div>
-        </div>
+            $numberOfRows = mysqli_num_rows($queryResult);
 
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-            <div class="card">
-            <img class="card-img-top" src="img/pants-7.jpeg" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title">Pants</h4>
-                <p class="card-text text-muted">The best pants ever.</p>
-                <p class="card-text">$88</p>
-                <a href="products.php" class="btn btn-primary">Buy</a>
-            </div>
-            </div>
-        </div>
+            if( $numberOfRows > 0 ){
 
-    </div>
+                echo "<div class='row'>";
+
+                while($rowArray = mysqli_fetch_assoc($queryResult)) {	
+
+                    $id = $rowArray["id"];
+                    $title = $rowArray["title"];
+                    $unformattedImage = $rowArray["image"];
+                    //image has ../, remove it
+                    $image = ltrim( $unformattedImage, "../" );
+                    $price = $rowArray["price"];
+                    //categories
+                    $categoryId = $rowArray["category_id"];
+                    $categoryName = $rowArray["category_name"];
+                    $lowerCaseCategory = strtolower($rowArray["category_name"]);
+                    echo "
+                        <div class='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                            <input type='hidden' name='".$id."' value='".$id."'>
+                            <input type='hidden' name='".$categoryId."' value='".$categoryId."'>
+                            <div class='card'>
+                            <img class='card-img-top' src='$image' alt='Card image'>
+                            <div class='card-body'>
+                                <h4 class='card-title'>$title</h4>
+                                <p class='card-text text-muted'>Category: $categoryName</p>
+                                <p class='card-text'>The best $lowerCaseCategory ever.</p>
+                                <p class='card-text'>$ $price</p>
+                                <a href='' class='btn btn-primary buy-btn'>Buy</a>
+                            </div>
+                            </div>
+                        </div>
+                        ";
+                }
+                echo "</div>";
+            }
+            else {
+                echo "<p>No info to display!</p>";
+            }
+        }
+        else {
+            echo "<p>Connection Failed</p>";
+        }
+
+    ?>
 </div>
 <!-- php footer -->
 <?php include "partials/footer.php"?>
