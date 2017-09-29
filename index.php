07e848a25220088fbd963e92c590ae60aced2d56
Nina Louw <ninalouw@mac.com>
2017-09-29 00:36:46 -0700

    add images to cards

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!-- php header -->
 <?php include "partials/header.php"?>
 <div class="container">
+    <!-- hero section -->
     <div class="hero-container">
         <div class="hero-flex-container">
             <div class="hero-item hero-one">
@@ -13,10 +14,63 @@
             <div class="hero-item hero-two">
                 <h1 class="center-hero"> The Latest Collection</h1>
                 <p class="center-hero-text text-md">Our newest fabric, is engineered to keep you feeling cool during super sweaty studio sessions.</p>
-                <button class="btn btn-primary"> View</button>
+                <button class="btn btn-primary">Shop Now</button>
             </div>
         </div>
     </div>
+    <!-- start of product cards -->
+    <h2 class="text-center">The Yoga Collection</h2>
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="card">
+            <img class="card-img-top" src="img/bra-1.jpeg" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Sports Bra</h4>
+                <p class="card-text text-muted">The best sports bra ever.</p>
+                <p class="card-text">$50</p>
+                <a href="products.php" class="btn btn-primary">Buy</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="card">
+            <img class="card-img-top" src="img/hoodie-1.jpeg" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Hoodie</h4>
+               <p class="card-text text-muted">The best hoodie ever.</p>
+                <p class="card-text">$50</p>
+                <a href="products.php" class="btn btn-primary">Buy</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="card">
+            <img class="card-img-top" src="img/pants-1.jpeg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Pants</h4>
+               <p class="card-text text-muted">The best pants ever.</p>
+                <p class="card-text">$88</p>
+                <a href="products.php" class="btn btn-primary">Buy</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="card">
+            <img class="card-img-top" src="img/pants-2.jpeg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Pants</h4>
+                <p class="card-text text-muted">The best pants ever.</p>
+                <p class="card-text">$88</p>
+                <a href="products.php" class="btn btn-primary">Buy</a>
+            </div>
+            </div>
+        </div>
+
+    </div>
+<!-- end of container -->
 </div>
 
 <!-- php footer -->
